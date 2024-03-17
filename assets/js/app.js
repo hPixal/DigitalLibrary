261a03978f8b368710e8a7790cca209077fa5e24
Lucas Saurin <lusaurin@gmail.com>
2024-03-17 12:43:09 -0300

    initial ReactTest2 commit (no changes)

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ExampleComponent from "../components/ExampleComponent.jsx";
+
+ReactDOM.render(<ExampleComponent />, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./assets/js/app.js",
+  output: {
+    path: path.resolve(__dirname, "public/build"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+};
