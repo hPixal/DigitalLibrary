92c1ba84884166a061424319cb363ebbf6e27d1b
Lucas Saurin <lusaurin@gmail.com>
2024-03-24 10:59:26 -0300

    .

--- assets/components/App.tsx
+++ assets/components/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import React from "react";
+import Bibliobar from "./Bibliobar";
 import "../styles/App.css";
 
 function App() {
--- assets/js/app.tsx
+++ assets/js/app.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "../components/App.tsx";
+import Bibliobar from "../components/Bibliobar.tsx";
 import "../styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <div>
+      <div>Hola crack que onda, aca andamos</div>
+      <div>Otro div aca por lasdudas, paso nomas de onda</div>
+    </div>
     <App />
   </React.StrictMode>
 );
